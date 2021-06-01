2c315c6880b0d9ec388c8391efd446c32a260540
Vivek Tak <57911834+wanderer-soul92@users.noreply.github.com>
2021-06-02 00:46:23 +0530

    Update bms_test_.py

--- bms_test_.py
+++ bms_test_.py
@@ -2,16 +2,18 @@ import unittest
 import stream_bms_data
 
 class test_bms_stream(unittest.TestCase):
-  def test_to_check_format(self):
-    self.assertTrue(stream_bms_data.isSuportedFormat('list') == False)
+    def test_to_check_format(self):
+        self.assertTrue(stream_bms_data.isSuportedFormat('list') == False)
     
     def test_to_check_BMS_allowed_range(self):
-        self.assertTrue(stream_bms_data.get_min_max_range({}) == "BMS_Allowed_Range is not defined")
+        self.assertTrue(stream_bms_data.get_min_max_range({} ) == "BMS_Allowed_Range is not defined")
 
-    def test_print_to_consol(self):
+    def test_print_to_consol_negative(self):
         self.assertTrue(stream_bms_data.print_to_consol({}, 'json') == "No Sensor Ouput" )
+
+    def test_print_to_consol_negative(self):
+        self.assertTrue(stream_bms_data.print_to_consol({'temperature': 44, 'soc': 70, 'charge_rate': 0}, 'json') )
 
 if __name__ == '__main__':
   unittest.main()
-
     
